9fb3803d68f4a9ceac867392a15df7abe555b3b0
TergBug <tergbug@gmail.com>
2019-12-07 18:33:34 +0200

    Initial commit

--- src/main/java/org/mycode/MyHashMap.java
+++ src/main/java/org/mycode/MyHashMap.java
@@ -3,19 +3,19 @@ package org.mycode;
 import java.util.ArrayList;
 
 public class MyHashMap {
-    private final static int downRange = 0;
-    private final static int upRange = 1000000;
+    private final static int DOWN_RANGE = 0;
+    private final static int UP_RANGE = 1000000;
     private ArrayList<Integer> map = new ArrayList<>();
     public void put(int key, int value){
-        if(key<downRange || key>upRange || value<downRange || value>upRange) return;
+        if(key< DOWN_RANGE || key> UP_RANGE || value< DOWN_RANGE || value> UP_RANGE) return;
         if(key>map.size()) for (int i = map.size(); i < key; i++) map.add(i, -1);
         map.add(key, value);
     }
     public int get(int key){
-        if(key<downRange || key>=map.size()) return -1;
+        if(key< DOWN_RANGE || key>=map.size()) return -1;
         return map.get(key);
     }
     public void remove(int key){
-        if(key>=downRange && key<map.size()) map.set(key, -1);
+        if(key>= DOWN_RANGE && key<map.size()) map.set(key, -1);
     }
 }
